05e12d01219dccea5a467dbb1a5732169bc117ec
juanmiguel431 <juanmiguel431@gmail.com>
2024-09-13 22:01:35 -0400

    getById - return type improved.

--- src/main/java/com/example/demo/controllers/StudentController.java
+++ src/main/java/com/example/demo/controllers/StudentController.java
@@ -33,7 +33,7 @@ public class StudentController {
     }
 
     @GetMapping("/students/{id}")
-    public ResponseEntity getById(@PathVariable int id) {
+    public ResponseEntity<?> getById(@PathVariable int id) {
         var student = students.stream().filter(s -> s.getId() == id).findFirst();
 
         if (student.isEmpty()) {
