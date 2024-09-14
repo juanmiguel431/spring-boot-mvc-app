da71db3250be49c4322c77b34b0106a48e071e9d
juanmiguel431 <juanmiguel431@gmail.com>
2024-09-14 11:05:45 -0400

    GlobalControllerAdvice moved.

--- src/main/java/com/example/demo/controllers/GlobalControllerAdvice.java
+++ src/main/java/com/example/demo/controllers/GlobalControllerAdvice.java
@@ -1,12 +1,13 @@
-package com.example.demo.models;
+package com.example.demo.controllers;
 
+import com.example.demo.models.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalControllerAdvice {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
