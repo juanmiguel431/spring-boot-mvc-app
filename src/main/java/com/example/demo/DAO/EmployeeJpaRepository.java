15aae8e83a4973d00ac28b196bf8a261bc92a99b
juanmiguel431 <juanmiguel431@gmail.com>
2024-09-14 22:39:40 -0400

    Spring boot data rest - Changing resource name and applying other configuration.

--- src/main/java/com/example/demo/DAO/EmployeeJpaRepository.java
+++ src/main/java/com/example/demo/DAO/EmployeeJpaRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.DAO;
 
 import com.example.demo.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeJpaRepository extends JpaRepository<Employee, Integer> {
 }
