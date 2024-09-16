83520c7baadcaab74afa1ea2f47baac809c60fea
juanmiguel431 <juanmiguel431@gmail.com>
2024-09-16 13:41:52 -0400

    Form Data binding - Text fields.

---
+++ src/main/java/com/example/demo/mvcControllers/StudentMvcController.java
@@ -0,0 +1,33 @@
+package com.example.demo.mvcControllers;
+
+import com.example.demo.models.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentMvcController {
+
+    @GetMapping("")
+    public String showForm(Model model) {
+
+        var student = new Student();
+        student.setFirstName("John");
+
+        model.addAttribute("model", student);
+
+        return "studentForm";
+    }
+
+    @PostMapping("")
+    public String processForm(Model model, @ModelAttribute Student student) {
+
+        model.addAttribute("model", student);
+
+        return "processStudentForm";
+    }
+}
