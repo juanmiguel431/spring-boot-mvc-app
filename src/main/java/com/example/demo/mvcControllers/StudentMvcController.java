eb9ee97f11992e13ee009cf30fb4679df8520582
juanmiguel431 <juanmiguel431@gmail.com>
2024-09-16 19:38:48 -0400

    Loop inside a View

--- src/main/java/com/example/demo/mvcControllers/StudentMvcController.java
+++ src/main/java/com/example/demo/mvcControllers/StudentMvcController.java
@@ -1,6 +1,7 @@
 package com.example.demo.mvcControllers;
 
 import com.example.demo.models.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,17 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/student")
 public class StudentMvcController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${languages}")
+    private List<String> languages;
 
     @GetMapping("")
     public String showForm(Model model) {
@@ -19,6 +28,8 @@ public class StudentMvcController {
         student.setFirstName("John");
 
         model.addAttribute("model", student);
+        model.addAttribute("countries", countries);
+        model.addAttribute("languages", languages);
 
         return "student-form";
     }
