f7d8558d72ec81b65d294159e9a8449b26b2aba9
juanmiguel431 <juanmiguel431@gmail.com>
2024-09-17 19:51:43 -0400

    CustomerMvcController - Trim white-spaces on every String.

--- src/main/java/com/example/demo/mvcControllers/CustomerMvcController.java
+++ src/main/java/com/example/demo/mvcControllers/CustomerMvcController.java
@@ -2,17 +2,23 @@ package com.example.demo.mvcControllers;
 
 import com.example.demo.models.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerMvcController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        var stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("")
     public String get(Model model) {
