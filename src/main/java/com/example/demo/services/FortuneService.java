ee80cc21fd6a6f3542bf66ab011108cfdff76b3a
juanmiguel431 <juanmiguel431@gmail.com>
2024-11-03 14:07:10 -0400

    TimeUnit.SECONDS.sleep is more readable.

--- src/main/java/com/example/demo/services/FortuneService.java
+++ src/main/java/com/example/demo/services/FortuneService.java
@@ -2,13 +2,15 @@ package com.example.demo.services;
 
 import org.springframework.stereotype.Service;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class FortuneService {
 
     public String getFortune(String name) throws Throwable {
 
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
+        TimeUnit.SECONDS.sleep(3);
 
         if (Objects.equals(name, "Juan Miguel")) {
             return "You are rich.";
